58dc9ee0d33f380710595a9d9c59474aa81a0243
Ali Nahid <alinahid@gmail.com>
2022-02-21 13:09:07 +1100

    download script

--- scripts/download-common-scripts.sh
+++ scripts/download-common-scripts.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+printf "\nStarting download script...\n"
 
-sourceUrl=
-scripts=("contains-element.sh", "extract-and-take-input.sh", "install-tanzu-framework.sh")
+if [[ -z $1 && -z $2 ]]
+then
+    printf "\nYou must provide necessary parameters eg: param#1 downloads-bundle-name and param#2 location\nif param#2 is not provide the default location is current directory.\n"
+fi
 
-for i in ${scripts[@]}; do
-    curl 
-done
+destinationDir='.'
+if [[ -n $2 ]]
+then
+    destinationDir=$2
+fi
+
+sourceUrl="https://raw.githubusercontent.com/alinahid477/common-merlin-scripts/main/scripts/"
+
+scripts=("contains-element.sh" "extract-and-take-input.sh" "install-tanzu-framework.sh")
+if [[ $1 == 'install-tanzu-framework' ]]
+then
+    scripts=("install-tanzu-framework.sh")
+fi
+
+printf "\nDestinatin DIR: $destinationDir\n"
+
+if [[ -d $destinationDir ]]
+then
+    cd $destinationDir
+    for i in ${scripts[@]}; do
+        printf "\ncurl -L -O $sourceUrl$i"
+        curl -L -O $sourceUrl$i
+    done
+    cd ~
+fi
+
