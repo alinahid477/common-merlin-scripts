3c1cf9c6cc7c040152b16601ba2a3e9fa05e6daf
Ali Nahid <alinahid@gmail.com>
2022-02-23 18:26:35 +1100

    wip

--- scripts/tanzu_connect_management.sh
+++ scripts/tanzu_connect_management.sh
@@ -30,8 +30,8 @@ function tanzu_connect_management () {
         ret=$?
         if [[ $ret -gt -1 ]]
         then
-            printf "\nERROR: Invalid option selected. Must require 1. Unable to connect.\n"
-            returnOrexit || return 1
+            printf "\nERROR: Invalid option selected. Unable to connect.\n"
+            returnOrexit && return 1
         else
             tanzuname=${tanzunames[$ret]}
         fi
@@ -75,19 +75,19 @@ function tanzu_connect_management () {
             if [[ -z $isexist ]]
             then
                 printf "\nERROR: kubeconfig not found in $kubeconfigfile\nExiting...\n"
-                returnOrexit || return 1
+                returnOrexit && return 1
             fi
             filename=$(ls -1tc $HOME/.config/tanzu/tkg/clusterconfigs/ | head -1)
             if [[ -z $filename ]]
             then
                 printf "\nERROR: Management cluster config file not found in $HOME/.config/tanzu/tkg/clusterconfigs/. Exiting...\n"
-                returnOrexit || return 1
+                returnOrexit && return 1
             fi
             clustername=$(cat $HOME/.config/tanzu/tkg/clusterconfigs/$filename | awk -F: '$1=="CLUSTER_NAME"{print $2}' | xargs)
             if [[ -z $clustername ]]
             then
                 printf "\nERROR: CLUSTER_NAME could not be extracted. Please check file ~/.config/tanzu/tkg/clusterconfigs/$filename. Exiting...\n"
-                returnOrexit || return 1
+                returnOrexit && return 1
             fi            
             contextname=$(parse_yaml $kubeconfigfile | grep "\@$clustername" | awk -F= '$1=="contexts_name"{print $2}' | xargs)    
             printf "\nfound \n\tCLUSTER_NAME: $clustername\n\tCONTEXT_NAME: $contextname\n"
@@ -111,7 +111,7 @@ function tanzu_connect_management () {
         read -p "Confirm to continue? [y/n] " yn
         case $yn in
             [Yy]* ) printf "\nyou confirmed yes\n"; echo "TANZU_CONNECT=YES" >> /tmp/TANZU_CONNECT; break;;
-            [Nn]* ) printf "\n\nYou said no. \n\nExiting...\n\n"; returnOrexit || return 1;;
+            [Nn]* ) printf "\n\nYou said no. \n\nExiting...\n\n"; returnOrexit && return 1;;
             * ) echo "Please answer yes or no.";;
         esac
     done  
