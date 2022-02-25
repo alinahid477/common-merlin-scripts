84ff3524ad11a2048f912be0fcb3d19003a3ff57
Ali Nahid <alinahid@gmail.com>
2022-02-26 10:53:02 +1100

    wip

--- scripts/download-common-scripts.sh
+++ scripts/download-common-scripts.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-scripts=("returnOrexit.sh" "contains-element.sh" "extract-and-take-input.sh" "install-tanzu-cli.sh" "init-prechecks.sh" "bastion_host_util.sh" "parse_yaml.sh" "select-from-available-options.sh" "tanzu_connect.sh")
 sourceUrl="https://raw.githubusercontent.com/alinahid477/common-merlin-scripts/main/scripts/"
 
 printf "\nStarting download script...\n"
 
-if [[ -z $1 && -z $2 ]]
-then
-    printf "\nYou must provide necessary parameters eg: param#1 downloads-bundle-name and param#2 location\nif param#2 is not provide the default location is current directory.\n"
-fi
+# if [[ -z $1 && -z $2 ]]
+# then
+#     printf "\nYou must provide necessary parameters eg: param#1 downloads-bundle-name and param#2 location\nif param#2 is not provide the default location is current directory.\n"
+# fi
 
 destinationDir='.'
 if [[ -n $2 ]]
@@ -16,12 +15,16 @@ then
     destinationDir=$2
 fi
 
+param1prompt="scripts download = all"
+readarray -t scripts < <(ls -1)
 if [[ $1 == 'install-tanzu-framework' ]]
 then
+    param1prompt="scripts download = $1"
     scripts=("install-tanzu-framework-tarfile.sh")
 fi
 
-printf "\nDestinatin DIR: $destinationDir\n"
+printf "\nDestinatin DIR = $destinationDir\n"
+printf "$param1prompt\n"
 
 if [[ -d $destinationDir ]]
 then
@@ -31,5 +34,7 @@ then
         curl -L -O $sourceUrl$i
     done
     cd ~
+else
+    printf "\nERROR: destination DIR does not exists\n"
 fi
 
--- scripts/returnOrexit.sh
+++ scripts/returnOrexit.sh
@@ -3,6 +3,20 @@
 returned='n'
 function returnOrexit()
 {
+
+
+    if [[ $1 == "kill shuttle" || $SSHUTTLE == true ]]
+    then
+        echo '=> Terminating sshuttle process by signal (SIGINT, SIGTERM, SIGKILL, EXIT)'
+        killall -9 sshuttle ssh
+        iptables --flush
+        sleep 2
+        iptables --flush
+        sleep 1
+        echo "=> *DONE*"
+        unset SSHUTTLE
+    fi
+
     returned='n'
     if [[ "${BASH_SOURCE[0]}" != "${0}" ]]
     then
