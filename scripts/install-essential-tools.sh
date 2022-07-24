0087d33e7d0f498e7502d57348e27398492af38b
Ali Nahid <alinahid@gmail.com>
2022-07-24 17:33:13 +1000

    added ytt

--- scripts/install-essential-tools.sh
+++ scripts/install-essential-tools.sh
@@ -8,6 +8,9 @@ function downloadKappCLI () {
     curl -L https://github.com/vmware-tanzu/carvel-kapp/releases/download/$(curl -s https://api.github.com/repos/vmware-tanzu/carvel-kapp/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/kapp-linux-amd64 -o $HOME/essential-clis/kapp
 }
 
+function downloadYttCLI () {
+    curl -L https://github.com/vmware-tanzu/carvel-ytt/releases/download/$(curl -s https://api.github.com/repos/vmware-tanzu/carvel-ytt/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/ytt-linux-amd64 -o $HOME/essential-clis/ytt
+}
 
 function installEssentialTools() {
 
@@ -34,6 +37,16 @@ function installEssentialTools() {
     then
         install $HOME/essential-clis/kapp /usr/local/bin/kapp
         chmod +x /usr/local/bin/kapp
+    fi
+
+    if [[ ! -f $HOME/essential-clis/ytt && ! -f /usr/local/bin/ytt ]]
+    then
+        downloadYttCLI
+    fi
+    if [[ ! -f /usr/local/bin/ytt ]]
+    then
+        install $HOME/essential-clis/ytt /usr/local/bin/ytt
+        chmod +x /usr/local/bin/ytt
     fi
 }
 
