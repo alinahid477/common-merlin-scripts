d2cfcf663b496950b39d08ecd8db907f964cbfc6
alinahid477 <ali@mint.com>
2023-08-24 21:06:11 +1000

    install tanzu cli standalone

--- scripts/install-tanzu-cli-standalone.sh
+++ scripts/install-tanzu-cli-standalone.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 version=v0.9.1
-downloadlink="https://github.com/vmware-tanzu/tanzu-cli/releases/download/$version/tanzu-cli-linux-amd64.tar.gz"
-curl -o /tmp/ -L $downloadlink
+filename=tanzu-cli-linux-amd64.tar.gz
+downloadlink="https://github.com/vmware-tanzu/tanzu-cli/releases/download/$version/$filename"
+printf "\ndownloading tanzu cli binary from github ($downloadlink)...\n"
+curl -o /tmp/$filename -L $downloadlink
+printf "\ndownloading merlin downloader...\n"
 curl -L https://raw.githubusercontent.com/alinahid477/common-merlin-scripts/main/scripts/download-common-scripts.sh -o /tmp/download-common-scripts.sh && chmod +x /tmp/download-common-scripts.sh
+printf "\ndownloading merlin tanzucli list...\n"
 /tmp/download-common-scripts.sh tanzucli /tmp/ && chmod +x /tmp/install-tanzu-cli.sh
+printf "\nexecuting /tmp/install-tanzu-cli.sh /tmp...\n"
 /tmp/install-tanzu-cli.sh /tmp
