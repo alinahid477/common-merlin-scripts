f5c152a974c2a19595d44883afd78f6c26e0b363
alinahid477 <ali@mint.com>
2024-02-18 20:06:41 +1100

    install tanzu cli

--- scripts/install-tanzu-cli.sh
+++ scripts/install-tanzu-cli.sh
@@ -131,7 +131,7 @@ installTanzuCLI () {
             return 1
         fi
         printf "Extracting $tanzuclibinary in $DIR....\n"
-        tar -xvf $tanzuclibinary -C $HOME/tanzu/ || return 1
+        tar -xvfz $tanzuclibinary -C $HOME/tanzu/ || return 1
         printf "$tanzuclibinary extracted in $DIR......COMPLETED.\n\n"
     fi
     sleep 1
